eeaa5b854e6b8f0f6fa3cb2170feed93a41a79da
don23id <don23id@yandex.ru>
2025-05-30 01:01:45 +0300

    Commit 2

--- collectible_catalog/settings.py
+++ collectible_catalog/settings.py
@@ -4,6 +4,7 @@ Django settings for collectible_catalog project.
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,10 +65,7 @@ WSGI_APPLICATION = 'collectible_catalog.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
